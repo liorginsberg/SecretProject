829b946baae5b65319e91e4c2c7adcebcff6363f
liorgins <lginsberg9a@gamil.com>
2013-10-08 00:55:54 +0300

    start working on design for messages

--- src/com/ebayplus/webapp/listeners/FetchTokensListener.java
+++ src/com/ebayplus/webapp/listeners/FetchTokensListener.java
@@ -45,96 +45,97 @@ public class FetchTokensListener implements ServletContextListener {
 
 		public void run() {
 
-			String devId = Global.getProperty("devId");
-			String certId = Global.getProperty("certId");
-			String appId = Global.getProperty("appId");
-			String runame = Global.getProperty("runame");
-			String signInURL = Global.getProperty("ebaySignInUrl");
-			String apiServerUrl = Global.getProperty("ebayAPIUrl");
-
-			// System.out.println("fetching tokens...");
-			AccountsDAO accountsDAO = AccountsDAO.getDAO();
-			List<Account> accounts = accountsDAO.getAllAccounts();
-			for (Account account : accounts) {
-				List<EbayPlusAccount> ebayPlusAccounts = account.getEbayPlusAccounts();
-				for (EbayPlusAccount ebayPlusAccount : ebayPlusAccounts) {
-					// System.out.println("fetching for accout with id " +
-					// account.getAccountID() + "and ebayAccountID = " +
-					// ebayPlusAccount.getAccountID());
-					if (ebayPlusAccount.getAccountStatus() == AccountStatusType.WAITING_FOR_ACTIVATION) {
-						// System.out.println("status is wating");
-						String sessionID = ebayPlusAccount.getAccountSession();
-						if (sessionID != null) {
-							ApiContext apiContext = Global.createApiContext(devId, appId, certId, apiServerUrl);
-							ApiLogging apiLogging = new ApiLogging();
-							apiContext.setApiLogging(apiLogging);
-
-							FetchTokenCall fetchTokenApiCall = new FetchTokenCall(apiContext);
-							fetchTokenApiCall.setSessionID(sessionID);
-							String token = null;
-							Calendar tokenExp = null;
-							try {
-								token = fetchTokenApiCall.fetchToken();
-								tokenExp = fetchTokenApiCall.getHardExpirationTime();
-							} catch (Exception e) {
-								System.out.println(e.getMessage());
-							}
-							if (token != null) {
-
-								/**
-								 * get user first... in order to know if this
-								 * user has been added already
-								 */
-
-								ApiCredential apiCredential = new ApiCredential();
-								apiCredential.seteBayToken(token);
-
-								apiContext.setApiCredential(apiCredential);
-
-								GetUserCall getUserApiCall = new GetUserCall(apiContext);
-
-								UserType user = null;
-
-								try {
-									user = getUserApiCall.getUser();
-								} catch (ApiException e) {
-									e.printStackTrace();
-								} catch (SdkException e) {
-									e.printStackTrace();
-								} catch (Exception e) {
-									e.printStackTrace();
-								}
-
-								if (user != null) {
-									boolean duplicate = false;
-									for(EbayPlusAccount otherAccount: ebayPlusAccounts) {
-										if(user.getUserID().equals(otherAccount.getAccountName())) {
-											otherAccount.setAccountToken(token);
-											ebayPlusAccount.setAccountStatus(AccountStatusType.DISABLED);
-											ebayPlusAccount.setAccountSession(null);
-											duplicate = true;
-											break;
-										} 
-									}
-									if(!duplicate) {
-										ebayPlusAccount.setAccountToken(token);
-										ebayPlusAccount.setAccountSession(null);
-										ebayPlusAccount.setAccountStatus(AccountStatusType.ACTIVE);
-										ebayPlusAccount.setAccountName(user.getUserID());
-									}
-									
-								}
-
-//								SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DDTHH:MM:SS.SSSZ");
-//								System.out.println("got token with expiration: " + sdf.format(tokenExp.getTime()));
-								accountsDAO.updateAccount(account);
-							}
-						} else {
-							// System.out.println("No sessionID !!!");
-						}
-					}
-				}
-			}
+			System.out.println("fetching tokens...");
+//			String devId = Global.getProperty("devId");
+//			String certId = Global.getProperty("certId");
+//			String appId = Global.getProperty("appId");
+//			String runame = Global.getProperty("runame");
+//			String signInURL = Global.getProperty("ebaySignInUrl");
+//			String apiServerUrl = Global.getProperty("ebayAPIUrl");
+//
+//			// System.out.println("fetching tokens...");
+//			AccountsDAO accountsDAO = AccountsDAO.getDAO();
+//			List<Account> accounts = accountsDAO.getAllAccounts();
+//			for (Account account : accounts) {
+//				List<EbayPlusAccount> ebayPlusAccounts = account.getEbayPlusAccounts();
+//				for (EbayPlusAccount ebayPlusAccount : ebayPlusAccounts) {
+//					// System.out.println("fetching for accout with id " +
+//					// account.getAccountID() + "and ebayAccountID = " +
+//					// ebayPlusAccount.getAccountID());
+//					if (ebayPlusAccount.getAccountStatus() == AccountStatusType.WAITING_FOR_ACTIVATION) {
+//						// System.out.println("status is wating");
+//						String sessionID = ebayPlusAccount.getAccountSession();
+//						if (sessionID != null) {
+//							ApiContext apiContext = Global.createApiContext(devId, appId, certId, apiServerUrl);
+//							ApiLogging apiLogging = new ApiLogging();
+//							apiContext.setApiLogging(apiLogging);
+//
+//							FetchTokenCall fetchTokenApiCall = new FetchTokenCall(apiContext);
+//							fetchTokenApiCall.setSessionID(sessionID);
+//							String token = null;
+//							Calendar tokenExp = null;
+//							try {
+//								token = fetchTokenApiCall.fetchToken();
+//								tokenExp = fetchTokenApiCall.getHardExpirationTime();
+//							} catch (Exception e) {
+//								System.out.println(e.getMessage());
+//							}
+//							if (token != null) {
+//
+//								/**
+//								 * get user first... in order to know if this
+//								 * user has been added already
+//								 */
+//
+//								ApiCredential apiCredential = new ApiCredential();
+//								apiCredential.seteBayToken(token);
+//
+//								apiContext.setApiCredential(apiCredential);
+//
+//								GetUserCall getUserApiCall = new GetUserCall(apiContext);
+//
+//								UserType user = null;
+//
+//								try {
+//									user = getUserApiCall.getUser();
+//								} catch (ApiException e) {
+//									e.printStackTrace();
+//								} catch (SdkException e) {
+//									e.printStackTrace();
+//								} catch (Exception e) {
+//									e.printStackTrace();
+//								}
+//
+//								if (user != null) {
+//									boolean duplicate = false;
+//									for(EbayPlusAccount otherAccount: ebayPlusAccounts) {
+//										if(user.getUserID().equals(otherAccount.getAccountName())) {
+//											otherAccount.setAccountToken(token);
+//											ebayPlusAccount.setAccountStatus(AccountStatusType.DISABLED);
+//											ebayPlusAccount.setAccountSession(null);
+//											duplicate = true;
+//											break;
+//										} 
+//									}
+//									if(!duplicate) {
+//										ebayPlusAccount.setAccountToken(token);
+//										ebayPlusAccount.setAccountSession(null);
+//										ebayPlusAccount.setAccountStatus(AccountStatusType.ACTIVE);
+//										ebayPlusAccount.setAccountName(user.getUserID());
+//									}
+//									
+//								}
+//
+////								SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DDTHH:MM:SS.SSSZ");
+////								System.out.println("got token with expiration: " + sdf.format(tokenExp.getTime()));
+//								accountsDAO.updateAccount(account);
+//							}
+//						} else {
+//							// System.out.println("No sessionID !!!");
+//						}
+//					}
+//				}
+//			}
 		}
 
 	}
